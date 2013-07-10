deb7a42bf0e87a826d2b532d2f2db9439863d303
Michael Schnitzbauer <doclumbri666@doc.(none)>
2013-07-10 18:58:55 +0200

    upsate web.js with index.html read

--- web.js
+++ web.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+/*var buf = require('buf');*/
 
 var app = express.createServer(express.logger());
+
+fs.readFileSync('/home/doclumbri666/node-startup/index.html', 'utf-8', function (err, data) {
+   if (err) throw err;
+    console.log(data);
+    response.send(data);
+});
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
